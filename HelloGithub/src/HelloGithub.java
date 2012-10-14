04f747e9758a3e1200e2e7b040dd2526b36d695f
Leo Torkkola <ahaz0ra@gmail.com>
2012-10-14 03:40:18 -0700

    latest 3:39am

---
+++ HelloGithub/src/HelloGithub.java
@@ -0,0 +1,18 @@
+// random randoms
+public class HelloGithub 
+{
+	public static void main(String[] args)
+	{
+		String abc = "Hello Github";
+		
+		for (int i = 0; i < 5; i++)
+		{
+			System.out.print(abc);
+			for (int ex = 0; ex < i; ex++)
+			{
+				System.out.print("!");
+			}
+			System.out.println();
+		}
+	}
+}
